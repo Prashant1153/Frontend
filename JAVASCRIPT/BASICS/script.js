d0466c412cfaa7d9358e3bb4b6840cda2b53b86d
prashant pandey <prashantpandey9812@gmail.com>
2025-10-15 09:30:37 +0530

    for loop

--- JAVASCRIPT/BASICS/script.js
+++ JAVASCRIPT/BASICS/script.js
@@ -1,3 +1,16 @@
-console.log("Hello, World!");
-let a = 10;
-console.log(a);
+// let age ;
+// age = 19;
+// if (age >=13 && age <=19){
+//     console.log("Teenager");
+// }
+// else {
+//     console.log("Not a teenager");
+// }
+
+for(let i=0;i<4;i++){
+      let str = "";
+      for (let j=0;j<4-i;j++){
+        str += "*";
+      }
+      console.log(str)
+}
